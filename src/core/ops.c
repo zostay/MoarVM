4a6c4b71556e5b5d7f110319c9b51a0b1cbf47aa
Timo Paulssen <timonator@perpetuum-immobile.de>
2015-03-03 23:03:28 +0100

    introduce ops needed for udp sockets
    resolvehostname - builds an address like we use for udp packets
    recvfrom - receives a message (and address) into two buffers
    sendto - sends a message to an address

--- src/core/ops.c
+++ src/core/ops.c
@@ -7186,6 +7186,50 @@ static const MVMOpInfo MVM_op_infos[] = {
         { MVM_operand_write_reg | MVM_operand_int64, MVM_operand_read_reg | MVM_operand_obj }
     },
     {
+        MVM_OP_resolvehostname,
+        "resolvehostname",
+        "  ",
+        3,
+        0,
+        0,
+        0,
+        0,
+        { MVM_operand_write_reg | MVM_operand_obj, MVM_operand_read_reg | MVM_operand_str, MVM_operand_read_reg | MVM_operand_int64 }
+    },
+    {
+        MVM_OP_udpsocket,
+        "udpsocket",
+        "  ",
+        1,
+        0,
+        0,
+        0,
+        0,
+        { MVM_operand_write_reg | MVM_operand_obj }
+    },
+    {
+        MVM_OP_recvfrom,
+        "recvfrom",
+        "  ",
+        4,
+        0,
+        0,
+        0,
+        0,
+        { MVM_operand_write_reg | MVM_operand_obj, MVM_operand_read_reg | MVM_operand_obj, MVM_operand_read_reg | MVM_operand_int64, MVM_operand_read_reg | MVM_operand_obj }
+    },
+    {
+        MVM_OP_sendto,
+        "sendto",
+        "  ",
+        4,
+        0,
+        0,
+        0,
+        0,
+        { MVM_operand_read_reg | MVM_operand_obj, MVM_operand_read_reg | MVM_operand_obj, MVM_operand_read_reg | MVM_operand_int64, MVM_operand_read_reg | MVM_operand_obj }
+    },
+    {
         MVM_OP_sp_log,
         "sp_log",
         ".s",
@@ -7689,7 +7733,7 @@ static const MVMOpInfo MVM_op_infos[] = {
     },
 };
 
-static const unsigned short MVM_op_counts = 700;
+static const unsigned short MVM_op_counts = 704;
 
 MVM_PUBLIC const MVMOpInfo * MVM_op_get_op(unsigned short op) {
     if (op >= MVM_op_counts)
